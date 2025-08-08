5e4099ffe4205d5b9e3d281837c11139ee1454bc
AnKi-007 <156802360+AnKi-007@users.noreply.github.com>
2025-08-08 17:01:41 +0530

    Delete db_config.py

--- db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as sql
-
-def get_connection():
-    return sql.connect(
-        host='localhost',
-        user='root',
-        passwd='Db_JaiShreeRAM@01',
-        database='UPSC'
-    )
